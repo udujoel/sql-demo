e704eef29768c9282996a008d017bbc786f59d37
Joell <amamihechi.udu@decagon.dev>
2020-01-21 11:00:42 +0100

    Add project files.

---
+++ TestBank.cs
@@ -0,0 +1,13 @@
+﻿using System.Data.SqlClient;
+
+namespace sqlTest {
+    public class TestBank {
+
+        public void ConnectToBankDB() {
+            string conectionString = "Data Source=.;Initial Catalog=Console_Bank;Integrated Security=True;";
+
+            SqlConnection connection = new SqlConnection(conectionString);
+            string query = "INSERT INTO customer_table VALUE (@firstname, @lastname,@username, @password)";
+        }
+        }
+    }
