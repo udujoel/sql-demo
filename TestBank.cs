2787690bd7d922c4b8a7e9b5847386fe5f2e3dc6
Joell <amamihechi.udu@decagon.dev>
2020-01-22 11:35:33 +0100

    Added db conn keys

--- TestBank.cs
+++ TestBank.cs
@@ -8,25 +8,66 @@ namespace sqlTest {
             string conectionString = "Data Source=.;Initial Catalog=Console_Bank;Integrated Security=True";
 
             SqlConnection connection = new SqlConnection(conectionString);
-            string query = "INSERT INTO customer_table VALUES(@firstname, @lastname,@username, @password)";
-            SqlCommand command = new SqlCommand(query , connection);
+            //            string InsertQuery = "INSERT INTO customer_table VALUES(@firstname, @lastname,@username, @password)";
+            string SelectQuery = "SELECT * FROM customer_table";
+            //            SqlCommand sqlInsertCommand = new SqlCommand(InsertQuery , connection);
+            SqlCommand sqlSelectCommand = new SqlCommand(SelectQuery , connection);
 
-            var firstname = "James";
-            var lastname = "Bond";
-            var username = "jamesbond";
-            var password = "007";
-
-            command.Parameters.AddWithValue("@firstname" , firstname);
-            command.Parameters.AddWithValue("@lastname" , lastname);
-            command.Parameters.AddWithValue("@username" , username);
-            command.Parameters.AddWithValue("@password" , password);
+            string firstname = "";
+            string lastname = "";
+            string username = "";
+            string password = "";
+            int id;
+            //
+            //            Console.WriteLine("\n\n\n");
+            //
+            //            Console.WriteLine("Firstname: ");
+            //            firstname = Console.ReadLine();
+            //
+            //            Console.WriteLine("Lastname: ");
+            //            lastname = Console.ReadLine();
+            //
+            //            Console.WriteLine("Username: ");
+            //            username = Console.ReadLine();
+            //
+            //            Console.WriteLine("Password: ");
+            //            password = Console.ReadLine();
+            //
+            //            Console.WriteLine("Press enter to insert to DB");
+            //            Console.ReadKey();
+            //
+            //
+            //
+            //            sqlInsertCommand.Parameters.AddWithValue("@firstname" , firstname);
+            //            sqlInsertCommand.Parameters.AddWithValue("@lastname" , lastname);
+            //            sqlInsertCommand.Parameters.AddWithValue("@username" , username);
+            //            sqlInsertCommand.Parameters.AddWithValue("@password" , password);
 
             try {
                 connection.Open();
                 Console.WriteLine("Connection opened");
-                int value = command.ExecuteNonQuery();
+                //                int value = sqlInsertCommand.ExecuteNonQuery();
+                SqlDataReader reader = sqlSelectCommand.ExecuteReader();
+
+                while (reader.Read()) {
+                    id = reader.GetInt32(0);
+                    firstname = reader.GetString(1);
+                    lastname = reader.GetString(2);
+                    username = reader.GetString(3);
+                    password = reader.GetString(4);
+
+                    Console.WriteLine($"ID: {id}");
+                    Console.WriteLine($"Firstname: {firstname}");
+                    Console.WriteLine($"Lastname: {lastname}");
+                    Console.WriteLine($"Username: {username}");
+                    Console.WriteLine($"Password: {password}");
+                    }
+
+                Console.ReadKey();
+
 
                 }
+
             catch (Exception e) {
                 Console.WriteLine($"Error: {e.Message}");
                 //                throw;
